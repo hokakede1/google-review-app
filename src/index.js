2c399d52eea7cbb60694aa7f56bd884be52cb015
Huy Dang <hqdang97@gmail.com>
2018-04-23 23:03:50 -0600

    Start
    Setup Linting
    Setting up Login Page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import LoginPage from "./components/LoginPage";
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App>
+      <Switch>
+        <Route path="/" component={}/>
+        <Route path="/login" component={LoginPage}/>
+      </Switch>
+    </App>
+  </Router>
+  , document.getElementById('root'),
+);
 // registerServiceWorker();
