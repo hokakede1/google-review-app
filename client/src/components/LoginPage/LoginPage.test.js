ab1b909a36c2183ba894e8804ebb189197a7988b
Huy Dang <hqdang97@gmail.com>
2018-05-08 19:30:39 -0600

    configing

--- client/src/components/LoginPage/LoginPage.test.js
+++ client/src/components/LoginPage/LoginPage.test.js
@@ -6,8 +6,8 @@ import LoginPage from './LoginPage';
 configure({adapter: new Adapter()});
 
 describe('<LoginPage />', () => {
-    it('should render something', () => {
-        const wrapper = shallow(<LoginPage />);
-        expect(wrapper.contains(<h1></h1>)).toEqual(true);
+    const wrapper = shallow(<LoginPage />);
+    it('should render correctly', () => {
+        expect(wrapper).toMatchSnapshot()
     })
 })
