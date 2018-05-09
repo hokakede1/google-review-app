59589b9c09f4e52857369cda531a7e7983762971
Huy Dang <hqdang97@gmail.com>
2018-05-09 09:10:04 -0600

    config database

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 function App() {
-  return <h1>jhello</h1>;
+	return <h1 />;
 }
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const massive = require('massive');
+const config = require('./config');
 
-// DB Setup
-mongoose.connect('mongodb://localhost:27017/auth');
 const app = express();
+// DB Setup
 
+massive(config.connectionString)
+	.then(db => {
+		app.set('db', db);
+		// db.initTables.initTables()
+		// .then( response => {
+		//   console.log('User table init'); })
+
+		console.log('connected to the DB');
+	})
+	.catch(err => console.log(`[DB ERROR]`, err));
 
 // App setup
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
-const router = require('./router')(app);
+// const router = require('./router')(app);
 
 // Server Setup
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
-server.listen(port);
-console.log('server is listening on port', port);
+server.listen(port, () => {
+	console.log('server is running on port ' + port);
+});
