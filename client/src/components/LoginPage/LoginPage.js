76f4827035828ff0122c019e538de4deb144f679
Huy Dang <hqdang97@gmail.com>
2018-05-12 12:50:26 -0600

    clear out login page

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -3,40 +3,27 @@ import { Segment } from 'semantic-ui-react';
 import Radium from 'radium';
 
 const styles = {
-  wrapper: {
-    height: '100vh',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: 'linear-gradient(#ff9a9e,#fecfef)',
-  },
-  loginSegment: {
-    width: '100%',
-    height: '250px',
-    '@media (min-width: 500px)': {
-      width: '400px',
-      height: '300px',
-    },
-  },
-
+	wrapper: {
+		height: '100vh',
+		width: '100%',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		background: 'linear-gradient(#ff9a9e,#fecfef)'
+	},
+	loginSegment: {
+		width: '100%',
+		height: '250px',
+		'@media (min-width: 500px)': {
+			width: '400px',
+			height: '300px'
+		}
+	}
 };
 class LoginPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'hi',
-    };
-  }
-  render() {
-    return (
-      <div style={styles.wrapper}>
-        <Segment raised style={styles.loginSegment}>
-          <h1>{this.state.name}</h1>
-        </Segment>
-      </div>
-    );
-  }
+	render() {
+		return <div style={styles.wrapper} />;
+	}
 }
-// LoginPage = Radium(LoginPage)
+
 export default Radium(LoginPage);
